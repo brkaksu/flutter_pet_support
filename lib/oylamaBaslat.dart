2cb8256f891df403c4e06ca7df1ff4cedd775fdb
Enes Korkmaz <aeneskrkmz@gmail.com>
2021-02-04 22:42:18 +0300

    Kullanıcı anasayfa tasarlandı

---
+++ lib/oylamaBaslat.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OylamaBilgileri{
+  String sehir = " ";
+  String semt = " ";
+  int oySayisi = 0;
+  String kullaniciAciklama = " " ;
+
+  OylamaBilgileri({this.kullaniciAciklama, this.oySayisi, this.sehir, this.semt});
+}
